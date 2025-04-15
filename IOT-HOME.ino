86f6146a6f4ffdf18c09b54743f295e45601dd83
pedrolucas7i <pedrolucas.core7i@gmail.com>
2025-04-15 12:53:01 +0100

    Update

--- IOT-HOME.ino
+++ IOT-HOME.ino
@@ -4,18 +4,21 @@ static const int servoPin = 14;
 static const int left_LED = 33;
 
 Servo garagem;
-int current_value = 30;
+int current_value = 35;
 
 void setup() {
   Serial.begin(115200);
   garagem.setPeriodHertz(50);
   garagem.attach(servoPin, 500, 2400);
+  pinMode(left_LED, OUTPUT);
 }
 
 void loop() {
+  digitalWrite(left_LED, HIGH);
   opening();
   delay(2000);
   closing();
+  digitalWrite(left_LED, LOW);
   delay(2000);
 }
 
@@ -30,7 +33,7 @@ void closing() {
 
 
 void opening() {
-  for (int i=current_value; i>30; i--) {
+  for (int i=current_value; i>35; i--) {
     garagem.write(i);
     current_value = i;
     Serial.println(current_value);
